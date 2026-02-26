d16d72f9a3fe3f92d362c4b6446c9ecc0782d335
KatharinaMaubach <137302088+KatharinaMaubach@users.noreply.github.com>
2026-02-26 12:54:02 +0100

    Rename files

--- R/external_llm_functions.R
+++ R/external_llm_functions.R
@@ -1,3 +1,31 @@
+#' Login-Function for the Databoard API
+#'
+#' This function logs into the [Databoard-API](https://databoard.uni-muenster.de/) and returns a valid access token. Please contact the [Digital Media and Computational Methods](https://www.uni-muenster.de/Kowi/en/personen/jakob-juenger.shtml) research unit to acquire an user account. Please make sure to keep your login data and access token private.
+#'
+#' @param username Username for the Login
+#' @param pw Password for the Login
+#'
+#' @return The access Token saved within the system environment
+#' @export
+db_login <- function(username, pw) {
+  basepath <- "https://databoard.uni-muenster.de"
+  path <- "/token"
+  body <- list(username = username, password = pw)
+
+  res <- httr::POST(paste0(basepath, path), body = body, encode = "form")
+
+  if (httr::status_code(res) == 200) {
+    access_token <- httr::content(res, as = "parsed", type = "application/json")$access_token
+    settings <- list(DATABOARD_ACCESS_TOKEN = access_token)
+    do.call(Sys.setenv, settings)
+    message("Logged in, access token saved in system environment")
+    return(invisible(TRUE))
+  } else {
+    warning("Password or Username wrong", call. = FALSE)
+    return(invisible(FALSE))
+  }
+}
+
 #' Automated content summaries with llms
 #'
 #' This function uses the databoard service from the
