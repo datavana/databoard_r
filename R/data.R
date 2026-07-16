494fce35aaa9e4120d0ced8d7f2b21fbbd731b38
KatharinaMaubach <137302088+KatharinaMaubach@users.noreply.github.com>
2026-07-16 13:53:37 +0200

    Add data description and change prompt vignette.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Movie dataset
+#'
+#' An fabricated data set about different movies
+#'
+#'
+#' @format ## `movies`
+#' A data frame with 10 rows and 4 columns:
+#' \describe{
+#'   \item{id}{A running id number}
+#'   \item{name}{Name of the moview}
+#'   \item{abstract}{A short abstract, imagined from chatgpt based on the movie title}
+#'   \item{year}{A review of the movie, imagined from chatgtp based on the movie title}
+#' }
+#' #' @source Communication Department of the University of Münster (<k.maubach@uni-muenster.de> and <jakob.juenger@uni-muenster.de>).
