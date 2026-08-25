11fbdc0d39c6d49ae10fa83f738337cf9654ac37
KatharinaMaubach <137302088+KatharinaMaubach@users.noreply.github.com>
2026-08-25 11:55:59 +0200

    Add info to movie data file

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Movie dataset
+#' Movie Dataset
 #'
-#' An fabricated data set about different movies
+#' A fabricated data set containing information about different movies
 #'
 #'
 #' @format ## `movies`
@@ -11,4 +11,24 @@
 #'   \item{abstract}{A short abstract, imagined from chatgpt based on the movie title}
 #'   \item{year}{A review of the movie, imagined from chatgtp based on the movie title}
 #' }
-#' #' @source Communication Department of the University of Münster (<k.maubach@uni-muenster.de> and <jakob.juenger@uni-muenster.de>).
+#' @source Communication Department of the University of Münster (<k.maubach@uni-muenster.de> and <jakob.juenger@uni-muenster.de>).
+#'
+#' @name movies
+NULL
+
+#' Movie Codebook
+#'
+#' An example codebook for the movies dataset
+#'
+#'
+#' @format ## `movies_rules`
+#' A data frame with 6 rows and 3 columns:
+#' \describe{
+#'   \item{category}{A category for coding featuring different movie genres}
+#'   \item{description}{A description of the category to be coded}
+#'   \item{example}{An example of code that should be coded within this category}
+#' }
+#' @source Communication Department of the University of Münster (<k.maubach@uni-muenster.de> and <jakob.juenger@uni-muenster.de>).
+#'
+#' @name movies_rules
+NULL
