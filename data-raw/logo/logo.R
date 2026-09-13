f3098aa9a55b78d3b7c01fd603239396de9c3660
strohne <krimskram@freenet.de>
2026-09-13 11:31:25 +0200

    Rename logo file

--- data-raw/logo/logo.R
+++ data-raw/logo/logo.R
@@ -3,28 +3,29 @@
 #
 
 library(tidyverse)
+library(rsvg)
 library(hexSticker)
 
 
 s <- sticker(
-  "data-raw/logo/logo.png",
+  "data-raw/logo/logo_green.svg",
   package = "databoard",
 
   # Image positioning and sizing
   s_x = 1,                 # subplot x position
   s_y = 0.8,              # subplot y position
-  s_width = 0.5,           # subplot width
-  s_height = 0.5,          # subplot height
+  s_width = 0.6,           # subplot width
+  s_height = 0.6,          # subplot height
 
   # Package name styling
   p_size = 20,             # font size
   p_x = 1,                 # text x position
-  p_y = 1.4,               # text y position
+  p_y = 1.3,               # text y position
   p_color = "#4CAF50",     # text color
 
   # Hexagon styling
   h_fill = "white",      # hexagon fill color
-  h_color = "#4CAF50",     # hexagon border color
+  h_color = "#92D050",     # hexagon border color
   h_size = 2,
 
   filename = "data-raw/logo/logo_hex.png"
@@ -32,4 +33,4 @@ s <- sticker(
 
 plot(s)
 
-usethis::use_logo("man/logo_hex.png", geometry = "240x278", retina = TRUE)
+usethis::use_logo("data-raw/logo/logo_hex.png", geometry = "240x278", retina = TRUE)
